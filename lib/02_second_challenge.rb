d6debc1c6edf9696ca5a783186e12fb0007790b9
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 04:05:27 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -6,7 +6,10 @@ def second_challenge
 :hero_friends=>[]
       #your key/value pairs here!
       },
-    :capulet => {
+    :capulet => {:patriarch=>{},
+:matriarch=>{},
+:heroine=>{},
+:heroine_friends=>[]
       #your key/value pairs here!
       }}
 
